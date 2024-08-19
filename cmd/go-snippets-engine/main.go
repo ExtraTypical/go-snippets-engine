dd2f4d4c5d60276cdf3bf4469b20dd054c6ed20c
Micah Gray <contact@micahgray.me>
2024-08-19 16:02:43 -0400

    Key recorder working

--- cmd/go-snippets-engine/main.go
+++ cmd/go-snippets-engine/main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	hook "github.com/robotn/gohook"
 )
 
 func main() {
 
-	fmt.Println("Program running!")
+	fmt.Println("~ Program running! Leader key is " + `";"` + " ~")
+	fmt.Println("~ It's recommended that you don't store any " + "information that could be sensitive in nature as a snippet. ~")
 
 	eventHandler()
 }
 
 func eventHandler() {
+
 	fmt.Println("~ To close out this program, close the terminal window. ~")
+
 	eventChan := hook.Start()
 	defer hook.End()
 
@@ -23,20 +27,27 @@ func eventHandler() {
 
 	for input := range eventChan {
 
+		switch {
+
 		/* Only read if leader is pressed */
-		if input.Kind == hook.KeyDown && input.Keychar == ';' {
+		case input.Kind == hook.KeyDown && input.Keychar == ';':
 			read = true
-		}
+
+		case input.Kind == hook.KeyDown && input.Rawcode == 51:
+			if len(readString) > 0 {
+				readRune := []rune(readString)
+				readString = strings.TrimRight(readString, string(readRune[len(readRune)-1]))
+			}
+			fmt.Println("String is", readString)
 
 		/* Clear out on space pressed */
-		if read && input.Kind == hook.KeyDown && input.Keychar == ' ' {
+		case input.Kind == hook.KeyDown && input.Keychar == ' ':
 			readString = ""
 			read = false
-		}
 
 		/* If read is true, record string */
-		if read && input.Kind == hook.KeyDown {
-			readString = readString + string(input.Keychar)
+		case read && input.Kind == hook.KeyDown && input.Keychar != 0:
+			readString += string(input.Keychar)
 			fmt.Println("String is", readString)
 		}
 
@@ -45,5 +56,4 @@ func eventHandler() {
 			break
 		}
 	}
-
 }
